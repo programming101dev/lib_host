5dea9caccf0b2a8c6a228ef77e471ab4aa3f2f6f
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 13:27:27 -0700

    Enforce semantic wrapper boundaries

--- src/stdlib.c
+++ src/stdlib.c
@@ -15,8 +15,6 @@
  */
 
 #include "p101_host/p101_stdlib.h"
-#include "p101_host/p101_unistd.h"
-#include "p101_host/sys/p101_utsname.h"
 #include <p101_env/wrapper.h>
 
 /*
--- src/unistd.c
+++ src/unistd.c
@@ -14,9 +14,7 @@
  * limitations under the License.
  */
 
-#include "p101_host/p101_stdlib.h"
 #include "p101_host/p101_unistd.h"
-#include "p101_host/sys/p101_utsname.h"
 #include <p101_env/wrapper.h>
 
 /*
